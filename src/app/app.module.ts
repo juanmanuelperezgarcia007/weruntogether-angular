e39b89f433f8037d401b958443f55ef002c6bf0a
jymmyneutron <jymmyneutron007@gmail.com>
2019-02-07 16:01:58 +0100

    realizado los componenetes,header y footer

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { IndexComponent } from './index/index.component';
+import { RegistroComponent } from './registro/registro.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.routing';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { CarrerasComponent } from './carreras/carreras.component';
+import { TiendasComponent } from './tiendas/tiendas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent,
+    RegistroComponent,
+    NoticiasComponent,
+    CarrerasComponent,
+    TiendasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFontAwesomeModule,
+    RouterModule.forRoot(appRoutes)
+ 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,20 @@
+import{ Routes } from '@angular/router'
+import{ IndexComponent } from './index/index.component';
+import{ RegistroComponent } from './registro/registro.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { CarrerasComponent } from './carreras/carreras.component';
+import { TiendasComponent } from './tiendas/tiendas.component';
+
+
+
+
+export const appRoutes: Routes=[
+    {path: '', redirectTo: 'index', pathMatch: 'full'},
+    {path: 'index', component: IndexComponent},
+    {path: 'noticias', component: NoticiasComponent},
+    {path: 'carreras', component: CarrerasComponent},
+    {path: 'tiendas', component: TiendasComponent},
+    {path: 'registro', component: RegistroComponent},
+  
+
+]
