52a41c7b9dd2e6db8f3cb56ae45d2caafc67d0dd
juan manuel <juanmanuelperezgarcia@gmail.com>
2019-05-29 16:51:37 +0200

    favoritos y sus promesas

--- src/app/carreras/carreras.component.ts
+++ src/app/carreras/carreras.component.ts
@@ -1,6 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
-import { CarrerasService} from '../carreras-service'
+import { CarrerasService } from '../carreras-service'
 import { FormGroup, FormControl } from '@angular/forms'
 
 import { LoginService } from '../login.service';
@@ -10,68 +10,86 @@ import { LoginService } from '../login.service';
   styleUrls: ['./carreras.component.css']
 })
 export class CarrerasComponent implements OnInit {
- carrerasAll:any
- favoritosVacio:boolean
- carrerasDistance:any
- carrerasType:any
- form : FormGroup;
- favoritosLleno:boolean
-  constructor(private carrerasService:CarrerasService, private loginService:LoginService) {
-    this.favoritosVacio=true
-    this.favoritosLleno=false
-   }
+  carrerasAll: any
+  favoritosVacio: boolean
+  carrerasDistance: any
+  carrerasType: any
+  form: FormGroup;
+  favoritosLleno: boolean
+  tokenUsuario = localStorage.getItem('token')
+  listFavorite:any
+  constructor(private carrerasService: CarrerasService, private loginService: LoginService) {
+    this.favoritosVacio = true
+    this.favoritosLleno = false
+  }
+  public visible: boolean = true
 
   ngOnInit() {
-    this.form= new FormGroup({
+    this.form = new FormGroup({
       min: new FormControl(''),
       max: new FormControl(''),
-      type: new FormControl('' ),
+      type: new FormControl(''),
       date: new FormControl(''),
       city: new FormControl(''),
       province: new FormControl(''),
     })
-    
+
     this.mostrarCarreras()
   }
-  mostrarCarreras(){
+  mostrarCarreras() {
     this.carrerasService.getAllCarreras()
-    .then((res)=>{
-      console.log(res)
+      .then((res) => {
+        console.log(res)
 
-      this.carrerasAll = res
-    })
-  
+        this.carrerasAll = res
+      })
+
   }
- 
 
-  getFiltersRace(){
+
+  getFiltersRace() {
     this.carrerasService.getFilters(
       this.form.value.min,
       this.form.value.max,
       this.form.value.date,
       this.form.value.type,
       this.form.value.city,
-      this.form.value.province,)
-    
-    .then((res)=>{
-      console.log(res)
-      
-      this.carrerasAll = res
-      if(this.carrerasAll==0){
-        alert('No hay resultados vuelve a realizar una busqueda')
-      }
-    })
-    
+      this.form.value.province)
+
+      .then((res) => {
+        console.log(res)
+
+        this.carrerasAll = res
+        if (this.carrerasAll == 0) {
+          alert('No hay resultados vuelve a realizar una busqueda')
+        }
+      })
+
     // this.form.reset()
 
   }
 
-  favoritos(){
-    console.log(this.favoritosLleno)
-    this.favoritosVacio=!this.favoritosVacio
-    this.favoritosLleno=!this.favoritosLleno
-    console.log(this.favoritosLleno)
-  }
-  
+  postFavoritos(pid, id) {
+    console.log(id)
+    this.paintStarFavorites()
+    this.carrerasService.postFavorite(
+      pid, 
+      this.visible, 
+      this.tokenUsuario)
+      .then((res) => {
+        
+        this.paintStarFavorites()
+    })
 
+
+  }
+  paintStarFavorites() {
+    this.carrerasService.GetFavorite(
+    this.tokenUsuario
+    ).then((res)=>{
+      this.listFavorite= res
+      console.log(this.listFavorite)
+
+    })
+  }
 }
